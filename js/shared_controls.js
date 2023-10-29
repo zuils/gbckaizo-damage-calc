3d9b0b6ae981e37fe8c7296326209f431de01552
zuils <vijaylemon05@gmail.com>
2023-10-29 04:50:27 -0400

    This should fix it

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -475,16 +475,17 @@ $(".set-selector").change(function () {
 			pokeObj.find(".level").val(set.level);
 			pokeObj.find(".hp .evs").val((set.evs && set.evs.hp !== undefined) ? set.evs.hp : 0);
 			pokeObj.find(".hp .ivs").val((set.ivs && set.ivs.hp !== undefined) ? set.ivs.hp : 31);
-			pokeObj.find(".hp .dvs").val((set.dvs !== undefined && set.dvs.hp !== undefined) ? set.dvs.hp : (set.ivs !== undefined && set.ivs.hp !== undefined) ? calc.Stats.IVToDV(set.ivs.hp) : 15);
+			pokeObj.find(".hp .dvs").val((set.dvs !== undefined && set.dvs.hp !== undefined) ? set.dvs.hp : (set.ivs !== undefined && set.ivs.hp !== undefined) ? set.ivs.hp : 15);
 			for (i = 0; i < LEGACY_STATS[gen].length; i++) {
+				// console.log(set.ivs[LEGACY_STATS[gen][i]]);
 				pokeObj.find("." + LEGACY_STATS[gen][i] + " .evs").val(
 					(set.evs && set.evs[LEGACY_STATS[gen][i]] !== undefined) ?
 						set.evs[LEGACY_STATS[gen][i]] : ($("#randoms").prop("checked") ? 84 : 0));
 				pokeObj.find("." + LEGACY_STATS[gen][i] + " .ivs").val(
 					(set.ivs && set.ivs[LEGACY_STATS[gen][i]] !== undefined) ? set.ivs[LEGACY_STATS[gen][i]] : 31);
 				pokeObj.find("." + LEGACY_STATS[gen][i] + " .dvs").val(
-					(set.dvs !== undefined && set.dvs[LEGACY_STATS[gen][i]] !== undefined) ? set.dvs[LEGACY_STATS[gen][i]] : (set.ivs !== undefined && set.ivs[LEGACY_STATS[gen][i]] !== undefined) ? calc.Stats.IVToDV(set.ivs[LEGACY_STATS[gen][i]]) : 15);
-			}
+					(set.dvs !== undefined && set.dvs[LEGACY_STATS[gen][i]] !== undefined) ? set.dvs[LEGACY_STATS[gen][i]] : (set.ivs !== undefined && set.ivs[LEGACY_STATS[gen][i]] !== undefined) ? set.ivs[LEGACY_STATS[gen][i]] : 15);
+				}
 			setSelectValueIfValid(pokeObj.find(".nature"), set.nature, "Hardy");
 			var abilityFallback = (typeof pokemon.abilities !== "undefined") ? pokemon.abilities[0] : "";
 			if ($("#randoms").prop("checked")) {
